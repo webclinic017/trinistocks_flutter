fd08672ea2d726be4637f8d296a3404a7a573da4
Rishi Latchmepersad <latchmepersad@gmail.com>
2021-04-12 20:13:06 -0400

    added a bunch of widgets to homepage

---
+++ lib/apis/marketindexes.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../utilities/config.dart' as config;
+import 'package:intl/intl.dart';
+import 'dart:io';
+
+class MarketIndexes {
+  MarketIndexes() {}
+
+  static Future<List> fetchLast30Days() async {
+    //first get todays date
+    var today = new DateTime.now();
+    var formatter = new DateFormat('yyyy-MM-dd');
+    String todayDate = formatter.format(today);
+    // then get the date from 30 days ago
+    var monthBack = today.subtract(const Duration(days: 30));
+    String monthBackDate = formatter.format(monthBack);
+    String url =
+        'https://trinistocks.com/api/marketindices?start_date=$monthBackDate&end_date=$todayDate';
+    const apiToken = config.APIKeys.app_api_token;
+    final response =
+        await http.get(url, headers: {"Authorization": "Token $apiToken"});
+    List apiResponse = [];
+    if (response.statusCode == 200) {
+      apiResponse = json.decode(response.body);
+    } else {
+      throw Exception("Could not fetch API data from $url");
+    }
+    // sort the news by date
+    apiResponse.sort((a, b) => (b['date']).compareTo(a['date']));
+    // temp delay to display overlay
+    await Future.delayed(Duration(seconds: 2));
+    // return the data from the api request
+    return apiResponse;
+  }
+}
